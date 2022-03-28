3cdcda49dbd86b54b300bf513eb6649b84071603
Miku <miku@39.com>
2022-03-28 21:22:32 +0800

    push test commit2

--- src/main/java/com/miku/git/GitTest.java
+++ src/main/java/com/miku/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("master commit 1");
         System.out.println("hot-fix1");
         System.out.println("push test");
+        System.out.println("push test1");
     }
 }
